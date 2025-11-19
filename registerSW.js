a17436b3a4c6c920c3609e5bfccedc5cbb18b59a
ssjbanghello1 <ssjbanghello1@gmail.com>
2025-11-19 08:14:33 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/organizador-financiero-pro/sw.js', { scope: '/organizador-financiero-pro/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/WAPP/sw.js', { scope: '/WAPP/' })})}
